bfd08e59d4a58d9f53cd58a7ad416ab99c313b62
Florian Lercher <florian.lercher@tum.de>
2019-11-10 17:17:12 +0100

    Add more colors

--- colors.py
+++ colors.py
@@ -5,15 +5,15 @@ Color = Enum(
     names = [
         ("blue", 1),
         ("b", 1),
-        ("BLUE", 1),
         ("red", 2),
         ("r", 2),
-        ("RED", 2),
         ("green", 3),
         ("g", 3),
-        ("GREEN", 3),
         ("yellow", 4),
         ("y", 4),
-        ("YELLOW", 4),
+        ("orange", 5),
+        ("o", 5),
+        ("purple", 6),
+        ("p", 6)
     ]
 )
